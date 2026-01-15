ca8b2a81241fc6ad32691643ebfc26c8d891b78c
joemagnusbizdev <joemagnusbizdev@gmail.com>
2026-01-15 20:45:48 +0200

    geojson

--- src1/components/GeoJsonPreview.tsx
+++ src1/components/GeoJsonPreview.tsx
@@ -1,21 +1,21 @@
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
-import type { FeatureCollection } from "geojson";
 
 type Props = {
-  geojson: FeatureCollection;
+  geojson: any;
 };
 
 export default function GeoJsonPreview({ geojson }: Props) {
+  if (!geojson) return null;
+
   return (
     <div className="h-64 w-full border rounded overflow-hidden">
       <MapContainer
         style={{ height: "100%", width: "100%" }}
-        bounds={undefined}
+        center={[20, 0]}
         zoom={2}
         scrollWheelZoom={false}
       >
         <TileLayer
-          attribution="© OpenStreetMap"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <GeoJSON data={geojson} />
